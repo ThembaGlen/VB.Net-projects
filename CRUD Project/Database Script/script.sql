f36fdac2084d3d4b6168fd106c0473584f4a7b68
Themba The C# Prince <93078403+ThembaGlen@users.noreply.github.com>
2021-11-30 23:35:59 +0200

    Update script.sql

--- CRUD Project/Database Script/script.sql
+++ CRUD Project/Database Script/script.sql
@@ -4,7 +4,42 @@ USE [CRUD_DB]
 GO
 /****** Object:  Table [dbo].[Product]    Scri﻿USE [master]
 GO
+USE [CRUD_DB]﻿USE [master]
+GO
 USE [CRUD_DB]
+GO
+/****** Object:  Table [dbo].[Product]    Script Date: 05/07/2017 17:52:53 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nchar](200) NULL,
+	[Category] [nchar](150) NULL,
+	[Rate] [decimal](18, 2) NULL,
+	[OpeningStock] [int] NULL,
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nchar](200) NULL,
+	[Category] [nchar](150) NULL,
+	[Rate] [decimal](18, 2) NULL,
+	[OpeningStock] [int] NULL,
+CREATE TABLE [dbo].[Product](
+	[ProductID] [int] NOT NULL,
+	[ProductName] [nchar](200) NULL,
+	[Category] [nchar](150) NULL,
+	[Rate] [decimal](18, 2) NULL,
+	[OpeningStock] [int] NULL,
+ CONSTRAINT [PK_Product] PRIMARY KEY CLUSTERED 
+(
+	[ProductID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+INSERT [dbo].[Product] ([ProductID], [ProductName], [Category], [Rate], [OpeningStock]) VALUES (1, N'Iphone 7 (128 GB)                                                                                                                                                                                       ', N'iOS                                                                                                                                                   ', CAST(70000.00 AS Decimal(18, 2)), 20)
+INSERT [dbo].[Product] ([ProductID], [ProductName], [Category], [Rate], [OpeningStock]) VALUES (2, N'iPhone 5s (32 GB)                                                                                                                                                                                       ', N'iOS                                                                                                                                                   ', CAST(25000.00 AS Decimal(18, 2)), 30)
+
 GO﻿USE [master]
 GO
 USE [CRUD_DB]
