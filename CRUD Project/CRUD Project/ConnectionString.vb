c591647a336cd3149eaaf43451b47ed6bdd9264e
Themba The C# Prince <93078403+ThembaGlen@users.noreply.github.com>
2021-12-06 20:14:50 +0200

    Update ConnectionString.vb

--- CRUD Project/CRUD Project/ConnectionString.vb
+++ CRUD Project/CRUD Project/ConnectionString.vb
@@ -1,4 +1,13 @@
 ﻿Imports System.IO
 Module ConnectionString
     Public ReadOnly cs As String = "Data source=Raj-PC\SQLExpress;Initial Catalog=CRUD_DB;Integrated Security=True;"
+End Module﻿Imports System.IO
+Module ConnectionString
+    Public ReadOnly cs As String = "Data source=Raj-PC\SQLExpress;Initial Catalog=CRUD_DB;Integrated Security=True;"
+End Module﻿Imports System.IO
+Module ConnectionString
+    Public ReadOnly cs As String = "Data source=Raj-PC\SQLExpress;Initial Catalog=CRUD_DB;Integrated Security=True;"
+End Module﻿Imports System.IO
+Module ConnectionString
+    Public ReadOnly cs As String = "Data source=Raj-PC\SQLExpress;Initial Catalog=CRUD_DB;Integrated Security=True;"
 End Module
